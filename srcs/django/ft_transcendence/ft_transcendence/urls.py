bdc9264f1e6dd60761e49295ae31066aed747669
hadrien2509 <hadrien2509@gmail.com>
2023-11-23 17:09:54 +0100

    login start

--- srcs/django/ft_transcendence/ft_transcendence/urls.py
+++ srcs/django/ft_transcendence/ft_transcendence/urls.py
@@ -18,9 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("/", include("tournament.urls")),
     path("login/", include("login.urls")),
-    path("tournament/", include("tournament.urls")),
     path('admin/', admin.site.urls),
 ]
 
