330e17ca0d22622c64f0e2f85d34d3a17ab824b1
Arsene <a.rurangiza@gmail.com>
2024-02-13 14:42:19 +0100

    [MSTONE] oatuh2 - gets tmp code from oauth server

--- srcs/django/ft_transcendence/accounts/views.py
+++ srcs/django/ft_transcendence/accounts/views.py
@@ -3,8 +3,19 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.http import HttpResponse, HttpResponseBadRequest
 
-def signup_view(req):
+authorize_uri = "https://api.intra.42.fr/oauth/authorize?\
+	client_id=u-s4t2ud-c6b234d3edfab001ec93a17143651dcef4184d26390c68e82a5d64bd4ea6686e\
+	&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2Faccounts%2Fcallback%2F\
+	&scope=public\
+	&response_type=code\
+	&state="
+
+fromLogin = 'd56b699830e77ba53855679cb1d252da'
+fromSignup = '7d2abf2d0fa7c3a0c13236910f30bc43'
+
+def signup_v(req):
     if req.method == 'POST':
         form = UserCreationForm(req.POST)
         if form.is_valid():
@@ -13,9 +24,12 @@ def signup_view(req):
             return redirect('home:welcome')
     else:
         form = UserCreationForm()
-    return render(req, 'accounts/signup.html', {'form': form})
+    return render(req, 'accounts/signup.html', {
+        'form': form,
+        'authorize_uri': authorize_uri+fromSignup
+    })
 
-def login_view(req):
+def login_v(req):
     if req.method == 'POST':
         form = AuthenticationForm(data=req.POST)
         if form.is_valid():
@@ -27,9 +41,24 @@ def login_view(req):
                 return redirect('home:welcome')
     else:
         form = AuthenticationForm()
-    return render(req, 'accounts/login.html', {'form': form})
+    return render(req, 'accounts/login.html', {
+        'form': form,
+        'authorize_uri': authorize_uri+fromLogin,
+        'known_users': User.objects.all(),
+    })
 
-def logout_view(req):
+"""
+Callback function in case of login using Oauth2
+- gets code
+"""
+def callback(req):
+    if req.GET.get('code') is None:
+        return HttpResponseBadRequest("Bad Request: Missing 'code' parameter")
+    else:
+        tmp_code = req.GET.get('code')
+        return HttpResponse(f"<h1>The Temporary Code is: {tmp_code} </h1>")
+
+def logout_v(req):
     if req.method == 'POST':
         logout(req)
         return redirect('home:welcome')
