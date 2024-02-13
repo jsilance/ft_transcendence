7a56049a0a6d9e1733a68cd653e0f10bdf2f1e17
Arsene <a.rurangiza@gmail.com>
2024-02-13 16:42:25 +0100

    [FEAT] can login using 42 intra

--- srcs/django/ft_transcendence/accounts/views.py
+++ srcs/django/ft_transcendence/accounts/views.py
@@ -4,6 +4,12 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponse, HttpResponseBadRequest
+import requests
+from dotenv import load_dotenv
+import os
+
+# get acces to environment variables
+load_dotenv()
 
 authorize_uri = "https://api.intra.42.fr/oauth/authorize?\
 	client_id=u-s4t2ud-c6b234d3edfab001ec93a17143651dcef4184d26390c68e82a5d64bd4ea6686e\
@@ -48,15 +54,62 @@ def login_v(req):
     })
 
 """
-Callback function in case of login using Oauth2
-- gets code
+Callback for Oauth2 logic
+--
+catches tmp_code, exchanges it for an access token, use that
+token to get user informations
 """
 def callback(req):
-    if req.GET.get('code') is None:
+    temporary_code = req.GET.get('code')
+    if temporary_code is None:
         return HttpResponseBadRequest("Bad Request: Missing 'code' parameter")
+
+    o42 = oauth42()
+    token = o42.get_token(temporary_code)
+    data = o42.get_user_data(token)
+    intra_login = data.get('login')
+
+    if intra_login in str(User.objects.all()):
+        user = User.objects.get(username=intra_login)
+        login(req, user)
+        if 'next' in req.POST:
+            return redirect(req.POST.get('next'))
+        else:
+            return redirect('home:welcome')
     else:
-        tmp_code = req.GET.get('code')
-        return HttpResponse(f"<h1>The Temporary Code is: {tmp_code} </h1>")
+        return redirect('accounts:login')
+
+class oauth42:
+    # exchange temporary code for access token
+    def get_token(self, code):
+        url = 'https://api.intra.42.fr/oauth/token'
+        data = {
+            'grant_type': 'authorization_code',
+            'client_id': os.getenv('CLIENT_ID'),
+            'client_secret': os.getenv('CLIENT_SECRET'),
+            'code': code,
+            'redirect_uri': 'http://localhost:8000/accounts/callback/' # YOU..AAARG!
+        }
+        response = requests.post(url, data=data)
+        if response.status_code == 200:
+            token_data = response.json()
+            return token_data.get('access_token')
+        else:
+            # Handle error
+            return response
+
+    # use access token to access user data
+    def get_user_data(self, access_token):
+        # Make a request to the provider's API to get user information
+        headers = {'Authorization': f'Bearer {access_token}'}
+        response = requests.get('https://api.intra.42.fr/v2/me', headers=headers)
+
+        if response.status_code == 200:
+            user_data = response.json()
+            return user_data
+        else:
+            return None
+
 
 def logout_v(req):
     if req.method == 'POST':
