ff38600884bf3ed2456dc6234e14de387d972a5b
jsilance <juliensilance@hotmail.com>
2024-02-13 19:07:17 +0100

    cleaning

--- srcs/django/launch.sh
+++ srcs/django/launch.sh
@@ -10,10 +10,10 @@ do
 	fi
 done
 
-wget https://github.com/mrdoob/three.js/archive/master.zip
-unzip master.zip
-mv three.js-master threejs
-rm master.zip
+# wget https://github.com/mrdoob/three.js/archive/master.zip
+# unzip master.zip
+# mv three.js-master threejs
+# rm master.zip
 
 if [ $? -eq 0 ]; then
     echo "La connexion à la base de données PostgreSQL est réussie.";
