6a6c62f467c963c03fc35fe7f66c67a34018c09f
Arsene <a.rurangiza@gmail.com>
2024-02-16 19:45:35 +0100

    [FIX] unable to update profile pic + broken css due to bootstrap

--- srcs/django/ft_transcendence/accounts/forms.py
+++ srcs/django/ft_transcendence/accounts/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -8,3 +9,16 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
+
--- srcs/django/ft_transcendence/accounts/views.py
+++ srcs/django/ft_transcendence/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
@@ -167,4 +167,20 @@ def deleteprofile(request, username):
 
 @login_required(login_url='/accounts/login/')
 def editprofile(request):
-	return render(request, 'accounts/editprofile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Your account has been updated!')
+            return redirect('accounts:profile_edit')
+        messages.error(request, 'NOT VALID')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'accounts/editprofile.html', context)
