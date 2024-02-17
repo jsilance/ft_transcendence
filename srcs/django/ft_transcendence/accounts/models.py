d556b65f096100634be35b885ba30b16b1c415f8
Arsene <a.rurangiza@gmail.com>
2024-02-17 13:46:43 +0100

    [IMP] resizes all large profile images

--- srcs/django/ft_transcendence/accounts/models.py
+++ srcs/django/ft_transcendence/accounts/models.py
@@ -1,13 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
+from PIL import Image
 
-# Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.svg', upload_to='profile_pics')
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
     isstudent = models.BooleanField(default=False)
     friends = ArrayField(models.CharField(max_length=30), null=True, blank=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+    # resizes all large profile images
+    def save(self, *args, **kwargs):
+        super().save()
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
