799fa224e091d9d2f65b0af3c1ff3086553efa99
Arsene <a.rurangiza@gmail.com>
2024-02-22 11:21:11 +0100

    [IMP] adds FriendList and FriendRequest models

--- srcs/django/ft_transcendence/accounts/admin.py
+++ srcs/django/ft_transcendence/accounts/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, FriendList, FriendRequest
 
 # Register your models here.
 admin.site.register(Profile)
+
+class FriendListAdmin(admin.ModelAdmin):
+    list_filter = ['user']
+    list_display = ['user']
+    search_fields = ['user']
+    readonly_fields = ['user']
+
+    class Meta:
+        model = FriendList
+
+admin.site.register(FriendList, FriendListAdmin)
+
+class FriendRequestAdmin(admin.ModelAdmin):
+    list_filter = ['sender', 'receiver']
+    list_display = ['sender', 'receiver']
+    search_fields = ['sender', 'receiver']
+    search_fields = ['sender__username', 'sender_email', 'receiver_email', 'receiver_username']
+
+    class Meta:
+        model = FriendRequest
+
+admin.site.register(FriendRequest, FriendRequestAdmin)
--- srcs/django/ft_transcendence/accounts/models.py
+++ srcs/django/ft_transcendence/accounts/models.py
@@ -2,14 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField
 from PIL import Image
+from django.conf import settings
+from django.utils import timezone
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.png', upload_to='profile_pics')
     isstudent = models.BooleanField(default=False)
-    friends = ArrayField(models.CharField(max_length=30), null=True, blank=True)
     wins = models.IntegerField(default=0)
     losses = models.IntegerField(default=0)
+    creation_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f'{self.user.username} Profile'
@@ -22,3 +24,89 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
+class FriendList(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user")
+    friends = models.ManyToManyField(User, blank=True, related_name="friends")
+
+    def __str__(self):
+        return self.user.username
+    
+    def add_friend(self, account):
+        """
+        Add a new friend
+        """
+        if account not in self.friends.all():
+            self.friends.add(account)
+            self.save()
+    
+    def remove_friend(self, account):
+        """
+        Remove friend
+        """
+        if account in self.friends.all():
+            self.friends.remove(account)
+    
+    def unfriend(self, target):
+        """
+        Unfriend someone
+        """
+        my_friends_list = self # person terminating 
+        my_friends_list.remove_friend(target)
+
+        targets_friends_list = FriendList.objects.get(user=target)
+        targets_friends_list.remove_friend(self.user)
+
+    def is_mutual_friend(self, target):
+        """
+        Is this a friend
+        """
+        if target in self.friends.all():
+            return True
+        return False
+
+class FriendRequest(models.Model):
+    """
+    A friend request consists of two main parts:
+        1. SENDER:
+            - Person sending the friend request
+        2. RECEIVER:
+            - Person receiving the friend request
+    """
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="sender")
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name="receiver")
+    is_active = models.BooleanField(blank=True, null=False, default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.sender.username
+    
+    def accept(self):
+        """
+        Accept a friend request
+            Update both SENDER and RECEIVER friend lists
+        """
+        receiver_friend_list = FriendList.objects.get(user=self.receiver)
+        if receiver_friend_list:
+            receiver_friend_list.add_friend(self.sender)
+            sender_friend_list = FriendList.objects.get(user=self.sender)
+            if sender_friend_list:
+                sender_friend_list.add_friend(self.receiver)
+                self.is_active = False
+                self.save()
+    
+    def decline(self):
+        """
+        Decline a friend request (sent to you)
+            Is it "declined" by setting the "is_active" field to False
+        """
+        self.is_active = False
+        self.save()
+    
+    def cancel(self):
+        """
+        Cancel a friend request (you sent)
+            It is "cancelled" by setting the "is_active" field to False
+        """
+        self.is_active = False
+        self.save()
