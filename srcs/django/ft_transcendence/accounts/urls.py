63e4eaf65f759acebcd550813832f4cb464767fc
Arsene <a.rurangiza@gmail.com>
2024-02-26 10:54:20 +0100

    [IMP] can unfriend

--- srcs/django/ft_transcendence/accounts/urls.py
+++ srcs/django/ft_transcendence/accounts/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
 	path('profile/edit/', views.editprofile, name='profile_edit'),
     path('profile/<str:username>/delete/', views.deleteprofile, name='profile_delete'),
     path('friend_request/', views.send_friend_request, name='friend_request'),
+    path('friend_remove/', views.remove_friend, name='remove_friend'),
     path('cancel_friend_request/', views.cancel_friend_request, name='friend_request_cancel'),
     path('accept_friend_request/<str:friend_request_id>', views.accept_friend_request, name='friend_request_accept'),
     path('decline_friend_request/<str:friend_request_id>', views.decline_friend_request, name='friend_request_decline'),
--- srcs/django/ft_transcendence/accounts/views.py
+++ srcs/django/ft_transcendence/accounts/views.py
@@ -380,3 +380,23 @@ def cancel_friend_request(request):
             payload['response'] = "You must be authenticated to cancel a friend requests"
     
     return HttpResponse(json.dumps(payload), content_type="application/json")
+
+def remove_friend(request):
+    user = request.user
+    payload = {}
+    if request.method == "POST" and user.is_authenticated:
+        user_id = request.POST.get("receiver_user_id")
+        if user_id:
+            try:
+                removee = User.objects.get(pk=user_id)
+                friend_list = FriendList.objects.get(user=user)
+                friend_list.unfriend(removee)
+                payload['response'] = "Successfully removed that friend"
+            except Exception as e:
+                payload['response'] = f"Something went wrong: {str(e)}"
+        else:
+            payload['response'] = "There was an error. Unable to remove that friend"
+    else:
+        payload['response'] = "You must be authenticated to remove a friend"
+    
+    return HttpResponse(json.dumps(payload), content_type="application/json")
